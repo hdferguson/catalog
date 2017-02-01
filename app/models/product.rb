d1b4d40f263004dc95cac0ac164f328a3ac326b1
hdferguson <hdferguson@valdosta.edu>
2017-02-01 04:53:57 +0000

    Added carrierwave

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
+    mount_uploader :image_url, PictureUploader
     has_many :line_items
-
-  before_destroy :ensure_not_referenced_by_any_line_item
+    before_destroy :ensure_not_referenced_by_any_line_item
+    
   
     validates :title, :description, :image_url, presence: true
     validates :price, numericality: {greater_than_or_equal_to: 0.01} 
