18e0cb8f9c4a817315a155e1db18722b26e7a6f4
hdferguson <hdferguson@valdosta.edu>
2017-02-13 05:50:32 +0000

    Part 1 for project 5 complete and working

---
+++ app/assets/javascripts/channels/products.js
@@ -0,0 +1,9 @@
+(function() {
+  App.products = App.cable.subscriptions.create("ProductsChannel", {
+    connected: function() {},
+    disconnected: function() {},
+    received: function(data) { 
+        $(".store #main").html(data.html);
+    }
+  });
+}).call(this);
