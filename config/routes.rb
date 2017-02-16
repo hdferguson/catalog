e9c52c96ab9400351e368da6b3b13537bf577fa2
hdferguson <hdferguson@valdosta.edu>
2017-02-16 15:38:42 +0000

    putting what I have to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   get 'search', to: 'store#search'
+  mount ActionCable.server => '/cable'
   resources :line_items do
     put 'decrease', on: :member
     post 'decrease', on: :member
