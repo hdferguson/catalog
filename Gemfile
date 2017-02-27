cef646c74b4fc58c5b55d060cfe5e30b29c55dcf
hdferguson <hdferguson@valdosta.edu>
2017-02-27 23:07:01 +0000

    Done with 5.1

--- Gemfile
+++ Gemfile
@@ -52,6 +52,8 @@ gem 'devise'
 
 gem 'simple_form'
 
+gem 'pundit'
+
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+  include Pundit
   protect_from_forgery unless: -> {request.format.json?}
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   protected
 
       def configure_permitted_parameters
