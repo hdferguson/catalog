0b05b8f91e4c79f2b7ce1fc974420d01c2314cd3
hdferguson <hdferguson@valdosta.edu>
2017-02-27 23:13:01 +0000

    5.2.1 is good

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,13 +1,20 @@
 class SellersController < ApplicationController
   before_action :set_seller, only: [:edit, :update]
   before_action :authenticate_account!
+  
+  def pundit_user
+     current_account
+  end
+  
   # GET /sellers/1/edit
   def edit
+    authorize @seller
   end
 
   # PATCH/PUT /sellers/1
   # PATCH/PUT /sellers/1.json
   def update
+    authorize @seller
     respond_to do |format|
       if @seller.update(seller_params)
         format.html { redirect_to store_index_url, notice: "The profile of the buyer #{@seller.name} was successfully updated." }
