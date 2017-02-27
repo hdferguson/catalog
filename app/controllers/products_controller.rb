e6489c63d3d19363c5d8a23ed765181077df6d8e
hdferguson <hdferguson@valdosta.edu>
2017-02-27 23:28:50 +0000

    5.2.3 is good.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,37 +1,49 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_account!
+  
+  
+  def pundit_user
+    current_account
+  end
+  
   # GET /products
   # GET /products.json
   def index
-    if (params[:seller_id])
-      @seller = Seller.find(params[:seller_id])
-      @products = @seller.products
-    else
-      @products = Product.all
-    end 
+    #if (params[:seller_id])
+     # @seller = Seller.find(params[:seller_id])
+      #@products = @seller.products
+    #else
+     # @products = Product.all
+    #end
+    
+    authorize Product 
+    @products = policy_scope(Product)
   end
 
   # GET /products/1
   # GET /products/1.json
   def show
+    authorize @product
     @orders = @product.orders
   end
 
   # GET /products/new
   def new
     @product = Product.new
+    authorize @product
   end
 
   # GET /products/1/edit
   def edit
+    authorize @product
   end
 
   # POST /products
   # POST /products.json
   def create
     @product = Product.new(product_params)
-    
+    authorize @product
     if current_account && current_account.accountable_type == "Seller"
         @product.seller = current_account.accountable
     end
@@ -50,6 +62,7 @@ class ProductsController < ApplicationController
   # PATCH/PUT /products/1
   # PATCH/PUT /products/1.json
   def update
+    authorize @product
     respond_to do |format|
       if @product.update(product_params)
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
@@ -69,6 +82,7 @@ class ProductsController < ApplicationController
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
+    authorize @product
     @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
